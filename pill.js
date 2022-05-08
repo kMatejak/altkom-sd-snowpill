fdf6197f2f1f0bc12b752403014a9d2d0d909058
Krzysztof S. Matejak <44339684+kMatejak@users.noreply.github.com>
2022-05-08 16:15:27 +0200

    Create pill.js

---
+++ pill.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bdocument.getElementsByTagName("body")%5B0%5D.style.webkitAnimation %3D "none"%3Bdocument.getElementsByTagName("body")%5B0%5D.style.backgroundImage %3D "none"%7D)()
